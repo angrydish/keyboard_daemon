9aeba06e2788b891fe56cad0214cd50872e32100
nikita <yeliseev2003@mail.ru>
2023-05-21 07:06:11 +0700

    working, but not daemon

--- keyboard_backlight_daemon.sh
+++ keyboard_backlight_daemon.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
-#ASUS VivoBOOK S14 M433UA
+#ASUS VivoBook S14 M433UA
+rm -rf file.log
+xinput --test-xi2 --root > file.log & # process to trigger any keyboard/touchpad/mouse action
+
+while true
+do
+	start=`stat -c %y file.log | awk -F ':' '/1/{print $3}' | awk '{print $1}' FS=.` # detect seconds of file changing
+	if [ $(( SECONDS - start )) -gt 5 ]; then
+		echo 0 | sudo tee /sys/class/leds/asus::kbd_backlight/brightness 1>/dev/null # file to change the value of backlight
+	else
+		echo 3 | sudo tee /sys/class/leds/asus::kbd_backlight/brightness 1>/dev/null  # file to change the value of backlight
+	fi
+
+done
+
+#echo $(( SECONDS - start )) # difference between time
 
 
 
-#/sys/class/leds/asus::kbd_backlight/brightness - file to change the value of backlight, model 
 
-#start=$SECONDS - timer
-#echo $(( SECONDS - start )) - difference between time
 
-#xinput --test-xi2 --root > file.log & - process to trigger any keyboard/touchpad/mouse action
-#stat -c %y file.log | awk -F ':' '/1/{print $3}' | awk '{print $1}' FS=. - detect seconds of file changing
+
 
